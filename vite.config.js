309636e1316bb007545743667cc5d631dfa4e06b
OsniaaK <osniaak13@gmail.com>
2025-10-23 19:21:35 -0300

    test koyeb

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const isProduction = mode === 'production';
+  
+  const config = {
+    plugins: [react()],
+    define: {
+      'process.env': env,
+      'import.meta.env.VITE_BACKEND_URL': JSON.stringify(
+        isProduction 
+          ? 
+          (env.VITE_BACKEND_URL || 'http://localhost:4000') : ""
+      )
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: true,
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL || 'http://localhost:4000',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          configure: (proxy, _options) => {
+            proxy.on('error', (err, _req, _res) => {
+              console.log('proxy error', err);
+            });
+            proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log('Sending Request to the Target:', req.method, req.url);
+            });
+            proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+            });
+          }
+        }
+      }
+    }
+  };
+
+  return config;
+});
